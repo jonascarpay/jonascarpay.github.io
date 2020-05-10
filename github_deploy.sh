ee78ef96c7b327c0a9df7f8969d6a1ddfe2e75e7
Jonas <jonascarpay@gmail.com>
2020-05-10 21:08:46 +0900

    change abbrev

--- github_deploy.sh
+++ github_deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 SRC_BRANCH=source
-TRG_BRANCH=master
+TGT_BRANCH=master
 
 STATUS="$(git status --porcelain)"
 if [ -n "$STATUS" ]; then
@@ -15,7 +15,7 @@ git checkout $SRC_BRANCH
 MSG=$(git log -n 1 --format="format:%h %f")
 RESULT=$(nix-build)
 echo $RESULT
-git checkout $TRG_BRANCH
+git checkout $TGT_BRANCH
 rm -rf *
 cp -r $RESULT/* .
 chmod u+w . -R
