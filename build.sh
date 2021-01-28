9d46654f22d6c9156061a64c8c29a868c036610a
Jonas Carpay <jonascarpay@gmail.com>
2021-01-28 17:44:18 +0900

    Include metadata

--- build.sh
+++ build.sh
@@ -9,28 +9,29 @@ POSTS=$(find posts/ -type f | sort -r)
 
 for post in $POSTS; do
 
-	URL="${post%.md}.html"
+  URL="${post%.md}.html"
 
-	pandoc "$post" \
-		-o "$OUTDIR/$URL" \
-		--standalone \
-		-c ../style.css \
-		--include-before-body=static/back.html \
-		--include-after-body=static/back.html \
-		--include-after-body=static/footer.html
-	pandoc "$post" -t html --template=static/toc_entry -V url="$URL" >> "$TMP/toc.html"
+  pandoc "$post" \
+    -o "$OUTDIR/$URL" \
+    --standalone \
+    -c ../style.css \
+    --include-before-body=static/back.html \
+    --include-after-body=static/back.html \
+    --include-after-body=static/footer.html \
+    --include-in-header=static/meta.html
+  pandoc "$post" -t html --template=static/toc_entry -V url="$URL" >>"$TMP/toc.html"
 
-	pandoc "$post" -t plain --template=static/rss_entry -V url="$URL" >> "$TMP/rss_body"
+  pandoc "$post" -t plain --template=static/rss_entry -V url="$URL" >>"$TMP/rss_body"
 done
 
 pandoc static/toc_header.html "$TMP/toc.html" \
-	-o "$OUTDIR/index.html" \
-	--standalone \
-	--metadata title="jonas' blog" \
-	-c style.css \
-	--include-after-body=static/footer.html
+  -o "$OUTDIR/index.html" \
+  --standalone \
+  --metadata title="jonas' blog" \
+  -c style.css \
+  --include-after-body=static/footer.html
 
-cat > "$OUTDIR/rss.xml" << EOM
+cat >"$OUTDIR/rss.xml" <<EOM
 <rss version="2.0">
 	<channel>
 		<title>jonas' blog</title>
