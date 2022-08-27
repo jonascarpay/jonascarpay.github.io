fd37d3a00c768c508d4cd078883afe97fbe3690f
Jonas Carpay <jonascarpay@gmail.com>
2022-08-27 17:23:52 +0900

    add md-headerfmt

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# jonascarpay.github.io
+jonascarpay.github.io
+=====================
 
 Source for [jonascarpay.github.io](https://jonascarpay.github.io) and [jonascarpay.com](https://jonascarpay.com).
 [RSS](https://jonascarpay.com/rss.xml)
--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   description = "blog";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.md-headerfmt.url = "github:jonascarpay/md-headerfmt";
+  inputs.md-headerfmt.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-utils, md-headerfmt, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
       blog = pkgs.stdenv.mkDerivation {
@@ -17,5 +19,10 @@
       defaultApp = pkgs.writeShellScriptBin "serve-blog" ''
         ${pkgs.httplz}/bin/httplz ${blog}
       '';
+      devShell = pkgs.mkShell {
+        packages = [
+          md-headerfmt.defaultPackage.${system}
+        ];
+      };
     });
 }
